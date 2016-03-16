d8fe5c8e851e4ad3b400a3e99e30562e5a40e6f8
Enrique Pineda <enrique.pineda@gmail.com>
2016-03-16 00:33:16 -0400

    HumanCreation service introduced

---
+++ services/human-creation.js
@@ -0,0 +1,25 @@
+angular.module('runequest-character.services')
+.factory('HumanCreation', [ '$log', 'DieRoll', function($log, DieRoll) {
+    return {
+        generate:function() {
+            var c = {}
+
+            c.characteristics = []
+            c.characteristics['str'] = undefined
+            c.characteristics['con'] = undefined
+            c.characteristics['siz'] = undefined
+            c.characteristics['int'] = undefined
+            c.characteristics['pow'] = undefined
+            c.characteristics['dex'] = undefined
+            c.characteristics['cha'] = undefined
+
+            for (var attr in c.characteristics) {
+                var pool = [ DieRoll.d(6), DieRoll.d(6), DieRoll.d(6) ]
+
+                c.characteristics[attr] = pool.reduce(function(a, b) { return a + b }, 0)
+                $log.debug(pool, c.characteristics[attr])
+            }
+            return c
+        }
+    } // return
+}])

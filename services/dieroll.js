e77a2efb72ce4d1615a5aa7a11f4b140ec353e9a
Enrique Pineda <enrique.pineda@gmail.com>
2016-03-16 00:32:33 -0400

    rolls are now 1 through <SIDES> inclusive

--- services/dieroll.js
+++ services/dieroll.js
@@ -2,7 +2,7 @@ angular.module('runequest-character.services')
 .factory('DieRoll', [ function() {
     return {
         d:function(sides) {
-            return [Math.floor(Math.random() * sides)]
+            return Math.floor(Math.random() * sides) + 1
         }
     } // return
 }])
